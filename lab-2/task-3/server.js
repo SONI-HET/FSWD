0730f605f7242404bdb007fb3109056c8564041d
SONI-HET <21it155@charusat.edu.in>
2023-07-07 19:52:17 +0530

    port 3001

--- lab-2/task-3/server.js
+++ lab-2/task-3/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
-// document.write('<script type="text/javascript>" src="./script.js"></script>');
 http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
-}).listen(3000); //the server object listens on port 8080
+  res.write('Hello World!');
+  res.end();
+}).listen(3001);
+console.log('server listening on http://localhost:3001');
